a453cf515f2b70d350a2f7e40c61463b72a23fd7
gaoyuhengcn <gaoyuheng@yuheng.hl.cn>
2019-05-01 09:23:26 +0800

    初始化

---
+++ config.inc.php
@@ -0,0 +1,17 @@
+<?php
+//ucenter 配置文件
+define('UC_CONNECT', '');
+define('UC_DBHOST', 'localhost');
+define('UC_DBUSER', 'ucenter_dev');
+define('UC_DBPW', 'pSaiwCLBLkYKKXaj');
+define('UC_DBNAME', 'ucenter_dev');
+define('UC_DBCHARSET', 'utf8');
+define('UC_DBTABLEPRE', '`ucenter_dev`.uc_');
+define('UC_DBCONNECT', '0');
+define('UC_KEY', '123456789');
+define('UC_API', 'http://ucenter.yuheng.hl.cn/dev');
+define('UC_CHARSET', 'utf-8');
+define('UC_IP', '');
+define('UC_APPID', '1');
+define('UC_PPP', '20');
+?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+include './config.inc.php';
+include './uc_client/client.php';
+
+if (empty($_COOKIE['uid'])){
+	header('Location: /login.php');
+	}else{
+	echo $_COOKIE['uid']
+	}
+?>
